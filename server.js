7c17fee8ec2f21a210bd65cf4e74d7367cb6d80e
nguyen-dangn-otsv <nguyen.dangn@one-line.com>
2026-01-15 11:13:29 +0700

    feat: update image tag

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ app.get('/health', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('hehehehe')
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
